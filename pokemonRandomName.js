bed2681a58aea4e2c8e4c0f5ea36515ac37538d3
Suissa <jnascimento@gmail.com>
2016-08-17 14:05:20 -0300

    Correção de let para const

--- pokemonRandomName.js
+++ pokemonRandomName.js
@@ -1,6 +1,6 @@
 const Pokemons = require('./pokemonsArray.json')
 
 module.exports = () => {
-  let randomPokemon = (total) => (Math.random() * total | 0) + 1
+  const randomPokemon = (total) => (Math.random() * total | 0) + 1
   return Pokemons[randomPokemon(Pokemons.length)]
 }
